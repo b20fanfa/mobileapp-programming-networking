f3e998df81c425ff4cec178683992174b6fcc1e1
Fanny Falck <b20fanfa@student.his.se>
2021-04-27 12:09:53 +0200

    La till metoden så att man kan läsa in mountains.json filen i MainActivity.java.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -3,13 +3,29 @@ package com.example.networking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
 public class MainActivity extends AppCompatActivity {
 
     private WebView myWebView;
+
+    @SuppressWarnings("SameParameterValue")
+    private String readFile(String fileName) {
+        try {
+            //noinspection CharsetObjectCanBeUsed
+            return new Scanner(getApplicationContext().getAssets().open(fileName), Charset.forName("UTF-8").name()).useDelimiter("\\A").next();
+        } catch (IOException e) {
+            Log.e("==>", "Could not read file: " + fileName);
+            return null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
